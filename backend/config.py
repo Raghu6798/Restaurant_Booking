96f28b3536ff7d69b71e001092dd6b56e63b36e5
Raghu Nandan <144773863+Raghu6798@users.noreply.github.com>
2025-02-26 23:53:41 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-# API Keys
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-CEREBRAS_API_KEY = os.getenv("CEREBRAS_API_KEY")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-AI21_API_KEY = os.getenv("AI21_API_KEY")
