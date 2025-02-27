16e22d336ab1cb7b13a38d26e7679945a73dd00c
Raghu Nandan <144773863+Raghu6798@users.noreply.github.com>
2025-02-27 17:32:37 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/io5'],
-    },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
