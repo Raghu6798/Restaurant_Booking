b761abf0e4ca6a2cdc8ff04bc232d2355bf7c5fc
Raghu Nandan <144773863+Raghu6798@users.noreply.github.com>
2025-02-27 13:39:03 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -196,10 +196,14 @@ class DashboardState(BaseModel):
 # FastAPI app
 app = FastAPI()
 
+origins = [
+    "https://aiassistedrestaurantbooking.netlify.app",
+]
+
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
